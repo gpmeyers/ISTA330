4a806a8c3bf8317c973c8361938b178ca3fbe96d
Graysen Meyers <41168696+gpmeyers@users.noreply.github.com>
2021-02-01 22:45:32 -0700

    Question 12 Finished

--- practicum/week-2/question12.js
+++ practicum/week-2/question12.js
@@ -1,7 +1,6 @@
 /*
 A string, input, which contains only letters a and b is given.
-A list of substrings s1, s2, .., sn is called a partition for input if and only if
-  input == s1 + s2 + ... + sn.
+A list of substrings s1, s2, .., sn is called a partition for input if and only if input == s1 + s2 + ... + sn.
 
   
 A substring is balanced if it has equal number of a's and b's.
@@ -10,10 +9,31 @@ Among all the possible partitions of the input string, what is the maximum balan
 
 Example:
 input: 'abaabbabab'
-output: 4 because the following partition has the highest number of balanced substrings:
-          'ab', 'aabb', 'ab', 'ab'
+output: 4 because the following partition has the highest number of balanced substrings: 'ab', 'aabb', 'ab', 'ab'
 */
 
-var maxBalanceNumber = function(input) {
+var maxBalanceNumber = function (input) {
+  if (input.length == 0) {
+    return 0;
+  }
 
+  var a = 0;
+  var b = 0;
+
+  var answer = 0;
+
+  for (var i = 0; i < input.length; i++) {
+    if (input[i] == 'a') {
+      a++;
+    }
+    else if (input[i] == 'b') {
+      b++;
+    }
+
+    if (a == b) {
+      answer++;
+    }
+  }
+
+  return answer;
 };
